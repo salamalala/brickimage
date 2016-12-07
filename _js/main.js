f5206f1f56cc5fcf735a35e2354faf7e307a77c7
salamalala <hello@ninaregli.ch>
2016-12-07 17:44:46 +0100

    feat(scroll) make nav and subnav sticky when scroll

--- _js/main.js
+++ _js/main.js
@@ -1,33 +1,43 @@
 var smoothScroll = function() {
+
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
+          scrollTop: target.offset().top - 80
+        }, 800);
         return false;
       }
     }
   });
 };
 
-// var header = $("#header");
-//
-// header.on("scroll", function(e) {
-//
-//   if (this.scrollTop > 147) {
-//     header.addClass("fix-search");
-//   } else {
-//     header.removeClass("fix-search");
-//   }
-//
-// });
+var stickyHeaderElements = function(el, scrollPos) {
+
+  $(window).scroll(function(){
+    var stickyElement = $(el),
+        scroll = $(window).scrollTop();
+
+    if (scroll >= scrollPos) {
+      stickyElement.addClass('fixed');
+      console.log("fixed");
+    } else {
+      stickyElement.removeClass('fixed')
+    }
+  });
+};
+
+
 
 
 $(function() {
   if ( $('.subnav').length ) {
     smoothScroll();
+    stickyHeaderElements('.subnav__wrapper', 120);
   }
+
+  stickyHeaderElements('.nav', 120);
+
 });
