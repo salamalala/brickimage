b9616669fa2c94a909d3f7ccfd114bf6764db264
salamalala <hello@ninaregli.ch>
2017-01-03 17:52:01 +0100

    feat(*): change colors on scroll for subnavs

--- _js/main.js
+++ _js/main.js
@@ -1,6 +1,5 @@
 var menu = "close";
 var mobileNavigation = function() {
-
   $("#nav-mobile-js").click(function() {
 
     if (menu === "close") {
@@ -32,37 +31,13 @@ var smoothScroll = function() {
   });
 };
 
-var brickTinting = $("#brick-tinting").offset().top + (-100);
-var stoneTinting = $("#stone-tinting").offset().top + (-100);
-var specialProjects = $("#special-projects").offset().top + (-100);
-
-var subnavChangeColor = function() {
-  var $w = $(window).scroll(function(){
-      if ( $w.scrollTop() > specialProjects ){
-        $('#subnav_special').css({"color":"#E5AB24"});
-        $('#subnav_brick').css({"color":"#9F9894"});
-        $('#subnav_stone').css({"color":"#9F9894"});
-        console.log("hellospecial");
-      } else if ( $w.scrollTop() > stoneTinting ){
-        $('#subnav_stone').css({"color":"#E5AB24"});
-        $('#subnav_brick').css({"color":"#9F9894"});
-        $('#subnav_special').css({"color":"#9F9894"});
-        console.log("hellostone");
-      } else if ( $w.scrollTop() > brickTinting ) {
-          $('#subnav_brick').css({"color":"#E5AB24"});
-          $('#subnav_stone').css({"color":"#9F9894"});
-          $('#subnav_special').css({"color":"#9F9894"});
-          console.log("hellobrick");
-      }
-  });
-}
-
 var stickyHeaderElements = function(el, scrollPos) {
 
   $(window).scroll(function(){
     var stickyElement = $(el),
         scroll = $(window).scrollTop();
 
+    console.log("hello");
     if (scroll >= scrollPos) {
       stickyElement.addClass('fixed');
     } else {
@@ -71,23 +46,65 @@ var stickyHeaderElements = function(el, scrollPos) {
   });
 };
 
+var subnavExamplesColor = function() {
 
+  var brickTinting = $("#brick-tinting").offset().top + (-100);
+  var stoneTinting = $("#stone-tinting").offset().top + (-100);
+  var specialProjects = $("#special-projects").offset().top + (-100);
 
+  var $w = $(window).scroll(function(){
+      if ( $w.scrollTop() > specialProjects ){
+        $('#subnav_special').css({"color":"#E5AB24"});
+        $('#subnav_brick').css({"color":"#9F9894"});
+        $('#subnav_stone').css({"color":"#9F9894"});
+        console.log("special-projects");
+      } else if ( $w.scrollTop() > stoneTinting ){
+        $('#subnav_stone').css({"color":"#E5AB24"});
+        $('#subnav_brick').css({"color":"#9F9894"});
+        $('#subnav_special').css({"color":"#9F9894"});
+        console.log("stone-projects");
+      } else if ( $w.scrollTop() > brickTinting ) {
+        $('#subnav_brick').css({"color":"#E5AB24"});
+        $('#subnav_stone').css({"color":"#9F9894"});
+        $('#subnav_special').css({"color":"#9F9894"});
+        console.log("brick-projects");
+      }
+  });
+}
+
+var subnavAboutColor = function() {
+
+  var aboutTinting = $("#about-tinting").offset().top + (-100);
+  var aboutBrickImage = $("#about-brick-image").offset().top + (-100);
+
+  var $w = $(window).scroll(function(){
+      if ( $w.scrollTop() > aboutBrickImage){
+        $('#subnav-brick-image').css({"color":"#E5AB24"});
+        $('#subnav-tinting').css({"color":"#9F9894"});
+      } else if ( $w.scrollTop() > aboutTinting ){
+        $('#subnav-tinting').css({"color":"#E5AB24"});
+        $('#subnav-brick-image').css({"color":"#9F9894"});
+      }
+  });
+}
 
 
 $(function() {
   if ( $('.subnav').length ) {
     smoothScroll();
     stickyHeaderElements('.subnav__wrapper', 120);
+  }
+
+  if ( $('#subnav-examples').length ) {
     subnavChangeColor();
+  }
+
+  if ( $('#subnav-about').length ) {
+    subnavAboutColor();
   }
 
   stickyHeaderElements('.nav', 120);
 
   mobileNavigation();
-
-
-
-
 
 });
